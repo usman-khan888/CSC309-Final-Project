95eb93d10359ced4507c0cd44a598e19621e7f45
usman-khan888 <113370635+usman-khan888@users.noreply.github.com>
2025-04-09 12:46:47 -0400

    fixing logged out layout page

--- a3/frontend/src/components/Layout.jsx
+++ a3/frontend/src/components/Layout.jsx
@@ -2,20 +2,30 @@ import "./Layout.css";
 import { Link, Outlet } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 import PointsBadge from "../pages/PointsBadge";
+import { useEffect } from "react";
 
 const Layout = () => {
     const { user, logout } = useAuth();
+    
+    useEffect(() => {
+        if (user && user.role === undefined) {
+            logout();
+        }
+    }, [user, logout]);
+
     return <>
         <header>
             <Link to="/">Home</Link>
-            { user ? (
+            { user && user.role !== undefined ? (
                 <>
-                    {(user.role === 'cashier' ||  user.role === 'manager' || user.role === 'superuser')&& (
+                    {(user.role === 'cashier' || user.role === 'manager' || user.role === 'superuser') && (
                         <Link to="/register" className="register-btn">Register New User</Link>
                     )}
                     {user.role === 'regular' && <PointsBadge />}
                     {console.log(user.role)}
-                    <Link to="/edit-profile" className="edit-btn">Edit Profile</Link>
+                    {user.role !== undefined && (
+                        <Link to="/edit-profile" className="edit-btn">Edit Profile</Link>
+                    )}
                     <Link to="/profile" className="user">{user.utorid}</Link>
                     <a href="#" onClick={logout}>Logout</a>
                 </>
